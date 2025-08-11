55a95b1edae8194327c49c00cf8439632e07e40b
Justin Jarosz <justin15jarosz@gmail.com>
2025-08-11 16:12:58 -0400

    update

--- backend/user-service/src/config/database.js
+++ backend/user-service/src/config/database.js
@@ -10,9 +10,9 @@ class Database {
     this.pool = new Pool({
       host: process.env.DB_HOST || "localhost",
       port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME || "",
-      user: process.env.DB_USER || "",
-      password: process.env.DB_PASSWORD || "",
+      database: process.env.DB_NAME || "yardly",
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "postgres",
       max: 20,
       idleTimeoutMillis: 30000,
       connectionTimeoutMillis: 2000,
